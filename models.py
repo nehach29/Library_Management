badfb9c0219ef9bef265811ceb6008cbda08be67
Neha Chaudhary <109617232+nehach29@users.noreply.github.com>
2024-04-04 16:57:05 +0530

    models.py

--- models.py
+++ models.py
@@ -3,28 +3,40 @@ class manage_books:
     
     books = []     # Initialize an empty list called 'books' as a class attribute
 
-# Define a method named 'add_book' that takes three parameters: 'self', 'title', 'author', and 'isbn'
+# Define a method named 'add_book' that takes  parameters: 'self', 'title', 'author', and 'isbn'
     
     def add_book(self,title, author, isbn):
+
+        """
+        Add a new book to the collection.
+        
+        Args:
+            title (str): The title of the book to be added.
+            author (str): The author(s) of the book to be added.
+            isbn : The International Standard Book Number of the book to be added.
+        """
         self.books.append({"title": title, "author": author, "isbn": isbn})
         
 # Define a method named 'list_books' that takes only 'self' as a parameter
+        
     def list_books(self):
         for book in self.books:
-            print(book)     
+            print(book)      # Prints each book as a dictionary containing 'title', 'author', and 'isbn' keys.
 
 # Define a class named 'checkout_management'
+            
 class checkout_management:
     checkouts = []    # Initialize an empty list called 'checkouts' as a class attribute
 
  # Define a method named 'checkout_book' that takes three parameters
     
     def checkout_book(self,user_id, isbn):
-        return self.checkouts.append({"user_id": user_id, "isbn": isbn})
+        return self.checkouts.append({"user_id": user_id, "isbn": isbn})   #  Updated list of checkouts including the newly checked-out book.
 
 # Import the necessary modules
 import csv
 import os
+
 # Define a class named 'storage'
 class storage:
 
@@ -32,10 +44,8 @@ class storage:
     def store_books(self,title,author,isbn):
        
        file_name = r'C:\Users\nehaa\OneDrive\Desktop\codes\Logging.csv'     # Define the file path where the CSV file will be stored
-
-# Check if the file already exists
        
-       file_exists = os.path.isfile(file_name)
+       file_exists = os.path.isfile(file_name)   # # Check if the file already exists
 
  # Open the CSV file in append mode to add new records
        with open('Logging.csv','a',newline='') as f:
@@ -54,6 +64,6 @@ class storage:
 class user_management:
     users = []   # Initialize an empty list called 'users' as a class attribute
     
- # Define a method named 'add_user' to add a new user to the 'users' list
+ # Define a method named 'add_user' to add a new user to the collection
     def add_user(self,name, user_id):
         return self.users.append({"name": name, "user_id": user_id})
