0b5265a3605c6456902a4ec36c798e8e42b02460
Neha Chaudhary <109617232+nehach29@users.noreply.github.com>
2024-04-04 16:51:44 +0530

    book.py

--- book.py
+++ book.py
@@ -4,13 +4,12 @@ class manage_books:
     
     books = []     # Initialize an empty list called 'books' as a class attribute
 
-# Define a method named 'add_book' that takes three parameters: 'self', 'title', 'author', and 'isbn'
+# Define a method named 'add_book' that takes parameters: 'self', 'title', 'author', and 'isbn'
     
-    def add_book(self,title, author, isbn):
+    def add_book(self, title, author, isbn):
         self.books.append({"title": title, "author": author, "isbn": isbn})
         
 # Define a method named 'list_books' that takes only 'self' as a parameter
     def list_books(self):
         for book in self.books:
-            print(book)     
-
+            print(book)     #  Prints each book as a dictionary containing 'title', 'author', and 'isbn' keys.
